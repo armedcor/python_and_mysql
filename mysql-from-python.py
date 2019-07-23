81548265a0032a7837c5419f20d30d117ba568bd
Cormac Lawlor <armedcor@gmail.com>
2019-07-23 11:11:15 +0000

    Added new table and data

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,23 +1,23 @@
 import os
+import datetime
 import pymysql
 
-# Get username from Cloud9 workspace
+# Get the username from the Cloud9 workspace
 # (modify this variable if running on another environment)
 username = os.getenv('C9_USER')
 
 # Connect to the database
 connection = pymysql.connect(host='localhost',
-                            user=username,
-                            password='',
-                            db='Chinook')
-                            
+                             user=username,
+                             password='',
+                             db='Chinook')
+
 try:
-    # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-            print(row)
+    with connection.cursor() as cursor:
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
+        # Note that the above will still display a warning (not error) if the
+        # table already exists
 finally:
-    #Close the connect, regardless of wether the above was successful
     connection.close()
